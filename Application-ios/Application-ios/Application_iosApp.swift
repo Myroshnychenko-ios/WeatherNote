0c95323a20fdefe55477a93f1df20b34022774e7
Myroshnychenko Maxym <myroshnychenko@Myroshnychenkos-MacBook-Pro.local>
2026-02-05 22:57:00 +0200

    Project init

---
+++ Application-ios/Application-ios/Application_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Application_iosApp.swift
+//  Application-ios
+//
+//  Created by Myroshnychenko Maxym on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Application_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
