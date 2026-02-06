34418bc3a8b8fc7e3aead3fbb6687f7502894aee
Myroshnychenko Maxym <myroshnychenko@Myroshnychenkos-MacBook-Pro.local>
2026-02-06 16:54:21 +0200

    Implemented DI Container

---
+++ Application-ios/Application-ios/App/DependencyContainer.swift
@@ -0,0 +1,22 @@
+//
+//  DependencyContainer.swift
+//  Application-ios
+//
+//  Created by Myroshnychenko Maxym on 06.02.2026.
+//
+
+import Foundation
+import SwiftUI
+
+class DependencyContainer {
+    
+    static let shared = DependencyContainer()
+    
+    private let weatherService: WeatherAPIProtocol
+    
+    private init() {
+        let networkService = NetworkAPI(baseURL: URL(string: Constants.OpenWeather.urlString)!)
+        
+        self.weatherService = WeatherAPI(network: networkService)
+    }
+}
